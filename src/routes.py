3cdaa869e9dd169a588c6a30e4459a84a17affaf
daviirodrig <30713947+daviirodrig@users.noreply.github.com>
2021-11-12 22:12:57 -0300

    feature: genre endpoint

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from src.scrap import search_artist_genre
+from src.scrap import search_artist_genre, scrap_genre_page
 
 app = FastAPI()
 
@@ -9,3 +9,10 @@ def artist_genre_search(artist: str):
     result = search_artist_genre(artist)
 
     return {"artist": artist, "genres": result}
+
+
+@app.get("/genre")
+def genre_page(q: str):
+    result = scrap_genre_page(q.replace(" ", ""))
+
+    return result
--- src/scrap.py
+++ src/scrap.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from bs4.element import NavigableString
 import requests
 
 
@@ -11,3 +12,28 @@ def search_artist_genre(artist):
     links = bs.find_all("a")
 
     return [i.text for i in links[:-2]]
+
+
+def scrap_genre_page(genre):
+    url = f"https://everynoise.com/engenremap-{genre}.html"
+    str_html = requests.get(url).text
+    bs = BeautifulSoup(str_html, "html.parser")
+
+    divs = bs.find_all("div", class_="genre")
+    title_div = bs.find("div", class_="title")
+    final = {"artists": [], "playlists": []}
+
+    for div in divs:
+        final["artists"].append(
+            {
+                "name": div.text[:-2],
+                "preview_url": div.get("preview_url"),
+                "style": [i.strip() for i in div.get("style").split(";")],
+            }
+        )
+
+    for i in title_div.children:
+        if type(i) != NavigableString:
+            final["playlists"].append({i.text: i.attrs.get("href")})
+
+    return final
